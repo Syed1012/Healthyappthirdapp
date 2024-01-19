bd99d47c89c50fd8a29e448e5e22e4d04b2ac4f5
Syed Musaib <84576013+Syed1012@users.noreply.github.com>
2024-01-19 16:58:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  let foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", "Milk"];
+
+  return (
+    <>
+      <h1>Healthy Food</h1>
+      <ul class="list-group">
+        {foodItems.map((item) => (
+          <li class="list-group-item">{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
